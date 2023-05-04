081c046a4bd37bb16a830a5539fe0ca293febc32
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2023-05-03 21:06:29 -0700

    progressBar support fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,16 +18,12 @@ files {
     'ui/assets/fonts/*'
 }
 
-ui_page 'ui/index.html' -----------------------------------------------------------------------------
+ui_page 'ui/index.html'
 
-dependencies {
-    'vorp_core'
-}
-
-export "initiate"
+provide 'progressBars'
 
 --dont touch
-version '1.1'
+version '1.2.0'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_progressbar'
