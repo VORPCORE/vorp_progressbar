685390fef6794b02eed9ee86d357b13b3f167656
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:42:04 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,15 @@
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 game 'rdr3'
+
 lua54 'yes'
 author 'VORP @Bytesizd'
+description 'A tool to use within your scripts for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_progressbar'
 
-client_scripts {
-    'client/client.lua'
-}
+shared_scripts 'config.lua'
+client_scripts 'client/client.lua'
 
-shared_scripts {
-    'config.lua'
-}
 files {
     'ui/*',
     'ui/assets/*',
